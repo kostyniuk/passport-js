2848095c3c2c9a4fcb7c25bb4c9b5c85fe2f42a6
kostyniuk <kostyniuk7@gmail.com>
2020-06-24 15:43:04 +0300

    dress it up

--- local-strategy/app.js
+++ local-strategy/app.js
@@ -29,9 +29,11 @@ app.use(
   })
 );
 
+// forgetting about this require all the time
 require('./config/passport');
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use((req,res, next) => {
   console.log(req.session)
   console.log(req.user)
