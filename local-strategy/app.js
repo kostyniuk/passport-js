4a184c9561b7ae626d57af24730707e9d73300b7
kostyniuk <kostyniuk7@gmail.com>
2020-06-24 10:32:24 +0300

    add route

--- local-strategy/app.js
+++ local-strategy/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
-
 const app = express();
 
+const bodyParser = require('body-parser')
+
+const db = require('./config/db');
+
+const addUserRoute = require('./routes/addUser');
+
+require('dotenv').config()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+db()
+
 const PORT = process.env.PORT || 8000
+
+app.use('/addUser', addUserRoute)
 
 app.get('/', (req, res, next) => {
   res.json('home page')
 })
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
