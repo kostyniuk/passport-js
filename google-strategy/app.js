48393d47b0a7e98b524b9fc2fab0eb40341223b7
kostyniuk <kostyniuk7@gmail.com>
2020-06-24 17:26:13 +0300

    basic server

---
+++ google-strategy/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000
+
+
+
+app.listen(PORT, () => {
+  console.log( `Server has been started on http://localhost:${PORT}`)
+} ) 
