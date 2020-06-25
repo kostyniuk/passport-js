bde2e53fc93b62c99e39b9ee6bb563fcadfc0850
kostyniuk <kostyniuk7@gmail.com>
2020-06-25 18:02:03 +0300

    add parsers

--- google-strategy/app.js
+++ google-strategy/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const passport = require('passport');
 const app = express();
 
 require('dotenv').config();
@@ -8,9 +8,18 @@ const PORT = process.env.PORT || 8000;
 
 require('./config/passport');
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const loginRoute = require('./routes/login');
 const protectedRoute = require('./routes/protected');
+const signupRoute = require('./routes/signup');
 
+app.use(passport.initialize());
+
+app.use('/signup', signupRoute);
 app.use('/login', loginRoute);
 app.use('/protected', protectedRoute);
 
