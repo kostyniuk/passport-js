6566d2b13b6088eeaf3ec6c212f1412913336788
kostyniuk <kostyniuk7@gmail.com>
2020-06-30 11:04:50 +0300

    add google strategy

--- google-strategy/config/passport.js
+++ google-strategy/config/passport.js
@@ -1,22 +1,48 @@
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const JwtStrategy = require('passport-jwt').Strategy
-const ExtractJwt = require('passport-jwt').ExtractJwt
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const mongoose = require('mongoose');
+
+const GoogleUser = mongoose.model('UserGoogle');
 
 const parseProfile = require('../lib/parseGoogle');
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `http://localhost:${process.env.PORT}/login/google/callback`
-  },
-  function(accessToken, refreshToken, profile, done) {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `http://localhost:${process.env.PORT}/login/google/callback`,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const data = parseProfile(profile);
+      const { provider, providerId } = data;
 
-    console.log(profile)
+      const user = await GoogleUser.findOne({ provider, providerId });
+      console.log({ user });
+      if (user) {
+        console.log('User alredy exists');
+        return done(null, user);
+      } else {
+        const newUser = new GoogleUser(data);
+        const saved = await newUser.save();
+        console.log({ saved });
+        return done(null, saved);
+      }
+    }
+  )
+);
 
-    const data = parseProfile(profile)
-    console.log({data})
-
-    return done(null, {id: 3, name: 'Alex'})
-  }
-));
+passport.serializeUser(function (user, done) {
+  console.log('Serialization');
+  done(null, user.id);
+});
+passport.deserializeUser(function (obj, done) {
+  console.log('I wont have jack shit');
+  GoogleUser.findById(id, (err, user) => {
+    if (err) return done(err);
+    return done(null, user);
+  });
+});
