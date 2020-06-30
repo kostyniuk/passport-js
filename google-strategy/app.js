92b062b16786723ee55880095d62a07759955c95
kostyniuk <kostyniuk7@gmail.com>
2020-06-30 10:24:55 +0300

    adjust it

--- google-strategy/app.js
+++ google-strategy/app.js
@@ -1,31 +1,22 @@
 const express = require('express');
 const passport = require('passport');
-const app = express();
-
 require('dotenv').config();
+
+const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+require('./config/db');
+require('./models/UserGoogle');
+
 require('./config/passport');
-
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-const loginRoute = require('./routes/login');
-const protectedRoute = require('./routes/protected');
-const signupRoute = require('./routes/signup');
-
 app.use(passport.initialize());
 
-app.use('/signup', signupRoute);
-app.use('/login', loginRoute);
-app.use('/protected', protectedRoute);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('home page');
-});
+app.use(require('./routes'));
+// const bodyParser = require('body-parser');
 
 app.listen(PORT, () => {
   console.log(`Server has been started on http://localhost:${PORT}`);
