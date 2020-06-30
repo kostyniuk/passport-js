88656cca51be554327a093a598a87a5b840de5e7
kostyniuk <kostyniuk7@gmail.com>
2020-06-30 15:41:02 +0300

    add passport.authenticate

--- google-strategy/routes/protected.js
+++ google-strategy/routes/protected.js
@@ -2,8 +2,14 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json('protected page');
-});
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    res.json('protected page');
+    //   const token = jsonwebtoken.sign({ user: 'johndoe' }, jwtSecret);
+    // res.cookie('token', token, { httpOnly: true });
+  }
+);
 
 module.exports = router;
