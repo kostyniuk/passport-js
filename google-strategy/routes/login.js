1f6f7a6b81293abf567e6aa8943c1bbb90ef7e40
kostyniuk <kostyniuk7@gmail.com>
2020-06-30 15:40:42 +0300

    add logic to log in

--- google-strategy/routes/login.js
+++ google-strategy/routes/login.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const UserGoogle = mongoose.model('UserGoogle');
+const { validPassword } = require('../lib/passwordUtils');
 
-router.get('/', (req, res, next) => {
-  res.json('login page');
+router.post('/', async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserGoogle.findOne({ username });
+    if (!user) {
+      res.status(401).json({ success: false, msg: 'could not find user' });
+    }
+
+    const isValidPassword = await validPassword(password, user.password);
+
+    if (isValidPassword) {
+      // const tokenObject = utils.issueJWT(user);
+
+      res.status(200).json({ message: 'Successfully logged in' });
+
+      // res.status(200).json({ success: true, token: tokenObject.token, expiresIn: tokenObject.expires });
+    } else {
+      res
+        .status(401)
+        .json({ success: false, msg: 'you entered the wrong password' });
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  const token = jwt.sign({ user: 'johndoe' }, 'secret');
+  res.cookie('jwt', token, { httpOnly: true });
 });
 
 router.get(
@@ -21,7 +51,7 @@ router.get(
   }),
   (req, res) => {
     console.log('Redirected', req.user);
-    res.json({status: 'success', user: req.user})
+    res.redirect('/protected');
   }
 );
 
