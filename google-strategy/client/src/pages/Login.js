fca8d3d584a9c80e40897850665e3c1821359975
kostyniuk <kostyniuk7@gmail.com>
2020-07-07 18:45:43 +0300

    working on login component

--- google-strategy/client/src/pages/Login.js
+++ google-strategy/client/src/pages/Login.js
@@ -1,38 +1,73 @@
-import React from 'react';
-import { TextField, Button } from '@material-ui/core';
-import { Formik, Field, Form } from 'formik';
+import React, { useState } from 'react';
+import { TextField, Button, Grid } from '@material-ui/core';
+import styles from './../styles/login.module.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import MailIcon from '@material-ui/icons/Mail';
 
 const Login = () => {
+  const [form, setForm] = useState({username: '', password: ''});
+
+  const handleForm = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setForm(prev => ({...prev, [name]: value}))
+  };
+
   return (
     <div>
-      <Formik
-        initialValues={{ username: '', password: '' }}
-        onSubmit={() => {
-          console.log('The form is submitted!');
-        }}
+      <h1>Login</h1>
+      <Grid container direction='row' justify='center' alignItems='center'>
+        <TwitterIcon onClick={() => {console.log('twitter')}}/>
+        <MailIcon />
+      </Grid>
+      <h4>or</h4>
+      <form
+        className={styles.form}
+        noValidate
+        autoComplete='off'
+        onSubmit={() => console.log('submitted')}
       >
-        {({ values, isSubmitting, handleBlur, handleSubmit }) => (
-          <Form>
-            <h2>Login</h2>
-            <Field
-              placeholder='username'
-              name='username'
-              type='input'
-              as={TextField}
-            />
-            <div>
-              <Field
-                placeholder='password'
-                name='password'
-                type='input'
-                as={TextField}
-              />
-            </div>
-            <Button disabled={isSubmitting}>Submit</Button>
-            <pre>{JSON.stringify(values, null, 2)} </pre>
-          </Form>
-        )}
-      </Formik>
+        <TextField
+          id='filled-basic'
+          name='username'
+          className={styles.field}
+          label='username'
+          variant='filled'
+          onChange={(e) => handleChange(e)}
+        />
+        <div>
+          <TextField
+            id='filled-basic'
+          name='password'
+            className={styles.field}
+            label='password'
+            variant='filled'
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className={styles.btn}>
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={(e) => handleForm(e)}
+          >
+            Submit
+          </Button>
+          <Button
+            className={styles.btnReset}
+            variant='contained'
+            color='primary'
+          >
+            Reset
+          </Button>
+        </div>
+      </form>
+      <div style={{margin: '20px'}}>
+        {JSON.stringify(form, null, 2)}
+      </div>
     </div>
   );
 };
