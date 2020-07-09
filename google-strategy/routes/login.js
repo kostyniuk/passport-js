1297b184ef54f599a761bf9b2becfaa84633ff19
kostyniuk <kostyniuk7@gmail.com>
2020-07-09 20:22:19 +0300

    add a /api/login/failure endpoint to redirect here after

--- google-strategy/routes/login.js
+++ google-strategy/routes/login.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const UserGoogle = mongoose.model('UserGoogle');
 const { validPassword } = require('../lib/passwordUtils');
@@ -10,24 +9,22 @@ const issueJWT = require('../lib/issueJwt');
 router.post('/', async (req, res, next) => {
   const { username, password } = req.body;
 
-  console.log(req.body)
-
   try {
     const user = await UserGoogle.findOne({ username });
-    console.log({user})
-    console.log()
     if (!user) {
-      return res.status(401).json({ success: false, msg: 'could not find user' });
+      return res
+        .status(401)
+        .json({ success: false, msg: 'could not find user' });
     }
 
     const isValidPassword = await validPassword(password, user.password);
 
     if (isValidPassword) {
-      // const tokenObject = utils.issueJWT(user);
-
       const token = issueJWT(user);
       res.cookie('jwt', token, { httpOnly: true });
-      return res.status(200).json({ success: true, token, expiresIn: token.expires });
+      return res
+        .status(200)
+        .json({ success: true, token, expiresIn: token.expires });
     } else {
       res
         .status(401)
@@ -55,10 +52,13 @@ router.get(
     console.log('Redirected', req.user);
     const token = issueJWT(req.user);
     console.log({ token });
-    res.cookie('jwt', token, {httpOnly: true})
-    // res.json({ token: 'Bearer ' + token.token });
+    res.cookie('jwt', token, { httpOnly: true });
     res.redirect('/api/protected');
   }
 );
+
+router.get('/failure', (req, res, next) => {
+  res.json({ success: false, msg: 'Auth failed' });
+});
 
 module.exports = router;
