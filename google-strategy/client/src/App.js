30c52e11443ccce4547630b41fa1117775418efd
kostyniuk <kostyniuk7@gmail.com>
2020-07-10 14:22:39 +0300

    add context to an entire app

--- google-strategy/client/src/App.js
+++ google-strategy/client/src/App.js
@@ -11,28 +11,35 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Content from './pages/Content';
 import Home from './pages/Home';
+import AuthContext from './contexts/AuthContext';
+
+import useAuth from './hooks/useAuth';
 
 function App() {
+  const { info } = useAuth();
+
   return (
     <Router>
       <div className='App'>
-        <Switch>
-          <Route path='/' exact>
-            <div>
-              <Home />
-            </div>
-          </Route>
-          <Route path='/login' exact>
-            <Login />
-          </Route>
-          <Route path='/signup' exact>
-            <Signup />
-          </Route>
-          <Route path='/content' exact>
-            <Content />
-          </Route>
-          <Redirect to='/login' />
-        </Switch>
+        <AuthContext.Provider value={{ info }}>
+          <Switch>
+            <Route path='/' exact>
+              <div>
+                <Home />
+              </div>
+            </Route>
+            <Route path='/login' exact>
+              <Login />
+            </Route>
+            <Route path='/signup' exact>
+              <Signup />
+            </Route>
+            <Route path='/content' exact>
+              <Content />
+            </Route>
+            <Redirect to='/login' />
+          </Switch>
+        </AuthContext.Provider>
       </div>
     </Router>
   );
