2c2f9fdaa354d599772dec11582d15e63a91fbee
kostyniuk <kostyniuk7@gmail.com>
2020-07-10 17:38:33 +0300

    customize available pathes for either auth and not user

--- google-strategy/client/src/App.js
+++ google-strategy/client/src/App.js
@@ -12,37 +12,62 @@ import Signup from './pages/Signup';
 import Content from './pages/Content';
 import Home from './pages/Home';
 import AuthContext from './contexts/AuthContext';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import useAuth from './hooks/useAuth';
 
 function App() {
   const { info } = useAuth();
 
-  return (
-    <Router>
-      <div className='App'>
-        <AuthContext.Provider value={{ info }}>
-          <Switch>
-            <Route path='/' exact>
-              <div>
-                <Home />
-              </div>
-            </Route>
-            <Route path='/login' exact>
-              <Login />
-            </Route>
-            <Route path='/signup' exact>
-              <Signup />
-            </Route>
-            <Route path='/content' exact>
-              <Content />
-            </Route>
-            <Redirect to='/login' />
-          </Switch>
-        </AuthContext.Provider>
-      </div>
-    </Router>
-  );
+  if (!info.ready) {
+    return <CircularProgress />;
+  } else {
+    if (info.isAuthenticated) {
+      return (
+        <Router>
+          <div className='App'>
+            <AuthContext.Provider value={{ info }}>
+              <Switch>
+                <Route path='/' exact>
+                  <div>
+                    <Home />
+                  </div>
+                </Route>
+
+                <Route path='/content' exact>
+                  <Content />
+                </Route>
+                <Redirect to='/content' />
+              </Switch>
+            </AuthContext.Provider>
+          </div>
+        </Router>
+      );
+    } else {
+      return (
+        <Router>
+          <div className='App'>
+            <AuthContext.Provider value={{ info }}>
+              <Switch>
+                <Route path='/' exact>
+                  <div>
+                    <Home />
+                  </div>
+                </Route>
+                <Route path='/login' exact>
+                  <Login />
+                </Route>
+                <Route path='/signup' exact>
+                  <Signup />
+                </Route>
+                <Redirect to='/login' />
+              </Switch>
+            </AuthContext.Provider>
+          </div>
+        </Router>
+      );
+    }
+  }
 }
 
 export default App;
