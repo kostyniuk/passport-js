c8e66c39a776d4b0762d6cdb46b13e23e5ec1654
kostyniuk <kostyniuk7@gmail.com>
2020-07-10 14:22:27 +0300

    finish Content component

--- google-strategy/client/src/pages/Content.js
+++ google-strategy/client/src/pages/Content.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../contexts/AuthContext';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Redirect } from 'react-router-dom';
 
 const Content = () => {
-  return (
-    <div>
-      <h2>Secret content only authenticated users permitted to see!</h2>
-    </div>
-  );
+  const { info } = useContext(AuthContext);
+
+  if (!info.ready) {
+    return <CircularProgress />;
+  } else {
+    if (info.isAuthenticated) {
+      return (
+        <div>
+          <h2>Secret content only authenticated users permitted to see!</h2>
+          <div style={{ margin: '20px' }}>{JSON.stringify(info, null, 2)}</div>
+        </div>
+      );
+    } else {
+      return <Redirect to='/login' />
+    }
+  }
 };
 
 export default Content;
