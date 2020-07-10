aabbcfc4f3587ded0f9ac926b2db2753d09337d8
kostyniuk <kostyniuk7@gmail.com>
2020-07-10 14:23:16 +0300

    dress it up a little bit

--- google-strategy/client/src/pages/Home.js
+++ google-strategy/client/src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import useAuth from '../hooks/useAuth';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const Home = () => {
   const { info, loading } = useAuth();
   console.log({ info });
+  if (loading) {
+    return <CircularProgress />;
+  }
 
   return (
     <div>
       <h1>Home page</h1>
+      <h3>Hi, {info.username}</h3>
       <div style={{ margin: '20px' }}>{JSON.stringify(info, null, 2)}</div>
       <div style={{ margin: '20px' }}>{JSON.stringify(loading, null, 2)}</div>
     </div>
