5d717967b8dd57bee6439456367d3ece5fbfe0ca
kostyniuk <kostyniuk7@gmail.com>
2020-07-10 17:39:53 +0300

    add a logout btn + add some functionality

--- google-strategy/client/src/pages/Content.js
+++ google-strategy/client/src/pages/Content.js
@@ -1,24 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import AuthContext from '../contexts/AuthContext';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Redirect } from 'react-router-dom';
+import { Button, useScrollTrigger } from '@material-ui/core';
+import useFetch from '../hooks/useFetch';
 
 const Content = () => {
   const { info } = useContext(AuthContext);
 
+  const [redirect, setRedirect] = useState(false);
+
+  const { error, loading, request } = useFetch();
+
+  const handleLogout = async () => {
+    const responce = await request('/api/logout');
+    console.log({ responce });
+    setRedirect(true);
+  };
+
+  if (redirect) {
+    window.location.reload(true);
+  }
+
   if (!info.ready) {
     return <CircularProgress />;
   } else {
-    if (info.isAuthenticated) {
-      return (
-        <div>
-          <h2>Secret content only authenticated users permitted to see!</h2>
-          <div style={{ margin: '20px' }}>{JSON.stringify(info, null, 2)}</div>
-        </div>
-      );
-    } else {
-      return <Redirect to='/login' />
-    }
+    return (
+      <div>
+        <Button variant='contained' color='secondary' onClick={handleLogout}>
+          Logout
+        </Button>
+        <h2>Secret content only authenticated users permitted to see!</h2>
+        <div style={{ margin: '20px' }}>{JSON.stringify(info, null, 2)}</div>
+      </div>
+    );
   }
 };
 
