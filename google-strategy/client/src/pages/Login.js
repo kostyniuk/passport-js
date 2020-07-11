bb330b71e87cca8dc65447eb032b1ace68f7a849
kostyniuk <kostyniuk7@gmail.com>
2020-07-11 14:38:52 +0300

    add google auth

--- google-strategy/client/src/pages/Login.js
+++ google-strategy/client/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {useHistory} from 'react-router-dom'
 import { TextField, Button, Grid } from '@material-ui/core';
 import styles from './../styles/login.module.css';
 import TwitterIcon from '@material-ui/icons/Twitter';
@@ -6,36 +7,44 @@ import MailIcon from '@material-ui/icons/Mail';
 import useFetch from '../hooks/useFetch';
 
 const Login = () => {
-  const [form, setForm] = useState({username: '', password: ''});
-  const {error, loading, request } = useFetch()
+  const [form, setForm] = useState({ username: '', password: '' });
+  const { error, loading, request } = useFetch();
+  const history = useHistory()
 
   const handleForm = async (e) => {
     const params = {
       method: 'POST',
       body: JSON.stringify(form),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-    const loginResponce = await request('api/login', params)
+        'Content-Type': 'application/json',
+      },
+    };
+    const loginResponce = await request('api/login', params);
 
     if (loginResponce.success) {
       window.location.reload(true);
     }
-
   };
 
   const handleChange = (e) => {
-    const { name, value } = e.target
-    setForm(prev => ({...prev, [name]: value}))
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
   };
 
+  const googleAuthHandler = async (e) => {
+    console.log('google auth');
+    window.location.assign('/api/login/google') //so-so
+  };
   return (
     <div>
       <h1>Login</h1>
       <Grid container direction='row' justify='center' alignItems='center'>
-        <TwitterIcon onClick={() => {console.log('twitter')}}/>
-        <MailIcon className={styles.icons} />
+        <TwitterIcon
+          onClick={() => {
+            console.log('twitter');
+          }}
+        />
+        <MailIcon className={styles.icons} onClick={googleAuthHandler} />
       </Grid>
       <h4>or</h4>
       <form
@@ -55,7 +64,7 @@ const Login = () => {
         <div>
           <TextField
             id='filled-basic'
-          name='password'
+            name='password'
             className={styles.field}
             label='password'
             variant='filled'
@@ -79,9 +88,7 @@ const Login = () => {
           </Button>
         </div>
       </form>
-      <div style={{margin: '20px'}}>
-        {JSON.stringify(form, null, 2)}
-      </div>
+      <div style={{ margin: '20px' }}>{JSON.stringify(form, null, 2)}</div>
     </div>
   );
 };
