0e959017b81df7ce3a8c46817bfce7c8cd1a8028
kostyniuk <kostyniuk7@gmail.com>
2020-07-12 13:55:46 +0300

    final comit

--- google-strategy/app.js
+++ google-strategy/app.js
@@ -3,6 +3,8 @@ const passport = require('passport');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
+const path = require('path');
+
 require('dotenv').config();
 
 const app = express();
@@ -16,11 +18,19 @@ require('./config/passport')(passport);
 app.use(passport.initialize());
 app.use(morgan('tiny'));
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(require('./routes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server has been started on http://localhost:${PORT}`);
